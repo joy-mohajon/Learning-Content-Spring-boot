3ff80487a68d2878b14dbee5ad3ed7a93e60d3a4
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-06-08 11:25:07 +0600

    initial structure

---
+++ thymeleaf/dynamic-thymeleaf/src/main/java/com/example/initial_project/InitialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.initial_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialProjectApplication.class, args);
+	}
+
+}
---
+++ thymeleaf/dynamic-thymeleaf/src/test/java/com/example/initial_project/InitialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.initial_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
