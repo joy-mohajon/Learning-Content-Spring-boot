ea9c16d70169d1e3cae6f6452182e539be043a91
joy-mohajon <mohajon33@gmail.com>
2023-06-10 22:16:34 +0600

    using path variable

--- query-params__path-variable/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
+++ query-params__path-variable/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
@@ -4,7 +4,7 @@ import com.eazybytes.eazyschool.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,10 +13,17 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidaysController {
 
-    @GetMapping("/holidays")
-    public String displayHolidays(@RequestParam(required = false) boolean festival, @RequestParam(required = false) boolean federal, Model model) {
-        model.addAttribute("festival", festival);
-        model.addAttribute("federal", federal);
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model) {
+        if(display != null && display.equals("all")){
+            model.addAttribute("festival", true);           
+            model.addAttribute("federal", true);
+        }else if(display != null && display.equals("festival")){
+            model.addAttribute("festival", true);           
+        }else if(display != null && display.equals("federal")){
+            model.addAttribute("federal", true);           
+        }
+
         List<Holiday> holidays = Arrays.asList(
                 new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
                 new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
